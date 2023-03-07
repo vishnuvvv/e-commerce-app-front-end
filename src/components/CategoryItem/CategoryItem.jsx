196be03c3d2f202bd051986d0d497f8b2753ba9f
vishnu <vishnusathyanathan@gmail.com>
2023-03-08 00:29:26 +0530

    created announcement and slider functionalities are progressing

---
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CategoryItem.css"
+
+const CategoryItem = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CategoryItem
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+  {
+    id: 0,
+    img: "https://img.freepik.com/free-photo/pretty-shopping-woman-smiling-wearing-hat-isolated-green-background_231208-4932.jpg?w=1380&t=st=1678203920~exp=1678204520~hmac=a20d6be322e85eaa839ec0f7a7b808271d0865585cd3cfd7e62810b967e458c6",
+    title: "SUMMER SALE",
+    description : "DON'T COMPROMISE ON STYLE! GET FLAT 45% OFF FOR NEW ARRIVALS",
+    bg: "#e7bfff",
+  },
+  {
+    id: 1,
+    img: "https://img.freepik.com/free-photo/full-length-portrait-happy-excited-girl-bright-colorful-clothes-holding-shopping-bags-while-standing-showing-peace-gesture-isolated_231208-5946.jpg?w=1380&t=st=1678204130~exp=1678204730~hmac=ddde804c753da35de0c07f5799c733cbd66732ffb50e91a1aecc4805a025bdce",
+    title: "HOLI SALE",
+    description : "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS",
+    bg: "#aeffe4",
+  },
+  {
+    id: 2,
+    img: "https://img.freepik.com/free-photo/blonde-woman-with-perfect-wavy-hairstyle-pink-party-dress-posing-hight-heels_273443-1636.jpg?w=1380&t=st=1678209852~exp=1678210452~hmac=33b1d5ac196084b5c1376d0f7a2284e2932fce8ae00d4a804b2c6a7966d59cf3",
+    title: "BIG BILLION SALE",
+    description : "DON'T COMPROMISE ON STYLE! GET FLAT 60% OFF FOR NEW ARRIVALS",
+    bg: "#fff1ca",
+  },
+];
