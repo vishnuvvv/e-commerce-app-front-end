a615bf1c9896307369e1915e972e4c6d98af858a
vishnu <vishnusathyanathan@gmail.com>
2023-09-13 15:58:55 +0530

    added predux-persist

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store"; // Import both store and persistor
+import { PersistGate } from "redux-persist/integration/react"; // Import PersistGate
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    {/* Wrap your App component with PersistGate */}
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
