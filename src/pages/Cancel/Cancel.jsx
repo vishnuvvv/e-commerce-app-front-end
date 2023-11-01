210645bcd6fa21a9059871d7faa518183b481b67
vishnu <vishnusathyanathan@gmail.com>
2023-11-01 10:14:41 +0530

    madified success cancel page and created orders page

--- src/pages/Cancel/Cancel.jsx
+++ src/pages/Cancel/Cancel.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Announcement from "../../components/Announcement/Announcement";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/footer/Footer";
 
 const Cancel = () => {
   return (
-    <div>
-      <h1>Payment Failed!</h1>
-    </div>
+    <>
+      <Announcement />
+      <Navbar />
+      <div>
+        <h1>Payment Failed!</h1>
+      </div>
+      <Newsletter />
+      <Footer />
+    </>
   );
 };
 
--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-
+import Navbar from "../../components/Navbar/Navbar";
+import Announcement from "../../components/Announcement/Announcement";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import Footer from "../../components/footer/Footer";
 const Success = () => {
   const location = useLocation();
   console.log(location);
   return (
-    <div style={{ alignItems: "center", justifyContent: "center" }}>
-      <h1>Congratulations Payment successful !!!</h1>
-    </div>
+    <>
+      <Announcement />
+      <Navbar />
+      <div style={{ alignItems: "center", justifyContent: "center" }}>
+        <h1>Congratulations Payment successful !!!</h1>
+      </div>
+      <Newsletter />
+      <Footer />
+    </>
   );
 };
 
