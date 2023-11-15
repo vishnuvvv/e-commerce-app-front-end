6f902fcf429b3f75a6374d77f60f9ce5e5bccda8
vishnu <vishnusathyanathan@gmail.com>
2023-11-15 22:21:37 +0530

    modified

--- src/config/requestMethods.js
+++ src/config/requestMethods.js
@@ -6,7 +6,7 @@ const userData = localStorage.getItem("persist:root");
 const user = userData ? JSON.parse(userData).user : null;
 const currentUser = user ? JSON.parse(user).currentUser : null;
 const token = currentUser ? currentUser.accessToken : null;
-console.log(token);
+
 
 
 export const publicRequest = axios.create({
